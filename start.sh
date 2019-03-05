0b467c261ac711e7d9e58645b222acccbd02126b
Roy Li <me@dada.li>
2019-03-05 21:06:09 +0800

    add support for tls env

--- start.sh
+++ start.sh
@@ -8,23 +8,33 @@ CONF="/etc/snell-server.conf"
 # reuse existing config when the container restarts
 
 run_bin() {
+    echo "Running snell-server with config:\n"
+    cat ${CONF}
+
     ${BIN} --version
     ${BIN} -c ${CONF}
 }
+
 if [ -f ${CONF} ]; then
-    echo "Found existing config..."
-    run_bin
- else
-    if [ -z ${PSK} ]; then
-        PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
-        echo "Using generated PSK: ${PSK}"
-    else
-        echo "Using predefined PSK: ${PSK}"
-    fi
-    echo "Generating new config..."
-    echo "[snell-server]" >> ${CONF}
-    echo "listen = 0.0.0.0:9102" >> ${CONF}
-    echo "psk = ${PSK}" >> ${CONF}
-    echo "obfs = http" >> ${CONF}
-    run_bin
+    echo "Found existing config, rm it."
+    rm ${CONF}
 fi
+
+if [ -z ${PSK} ]; then
+    PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
+    echo "Using generated PSK: ${PSK}"
+else
+    echo "Using predefined PSK: ${PSK}"
+fi
+
+if [ -z ${OBFS} ]; then
+    OBFS=tls
+fi
+
+echo "Generating new config..."
+echo "[snell-server]" >> ${CONF}
+echo "listen = 0.0.0.0:9102" >> ${CONF}
+echo "psk = ${PSK}" >> ${CONF}
+echo "obfs = ${OBFS}" >> ${CONF}
+
+run_bin
